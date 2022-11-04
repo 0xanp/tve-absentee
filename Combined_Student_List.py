157bfb58ae91b50d7a94b60bb4eedf08908fbf6c
Binh An <anbinhpham110799@gmail.com>
2022-11-04 11:06:22 +0700

    implemented the logic to pull attendance and comments

--- Combined_Student_List.py
+++ Combined_Student_List.py
@@ -1,5 +1,8 @@
-import streamlit as st
+import collections
 import pandas as pd
+import html_to_json
+import json
+import time 
 from dotenv import load_dotenv
 import os
 from selenium import webdriver
@@ -15,14 +18,8 @@ MANAGER_PASSWORD = os.getenv("MANAGER_PASSWORD")
 CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
 GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
 
-st.set_page_config(
-    page_title="Tri Viet Education",
-    page_icon=":pager:",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
 
-def html_to_dataframe(table_header, table_data, course_name=None):
+def html_to_dataframe(driver, table_header, table_data, course_name=None):
     header_row = []
     df_data = []
     for header in table_header:
@@ -31,7 +28,33 @@ def html_to_dataframe(table_header, table_data, course_name=None):
         columns = row.find_elements(By.XPATH,"./td") # Use dot in the xpath to find elements with in element.
         table_row = []
         for column in columns:
-            table_row.append(column.text)
+            try:
+                name = column.find_element(By.XPATH, './b')
+                #table_row.append(name.text)
+                print(f"{name.text}")
+            except:
+                try:
+                    href = column.find_element(By.XPATH,"./div/a").get_attribute('href')
+                    driver.execute_script(href)
+                    time.sleep(.8)
+                    forms = driver.find_elements(By.CLASS_NAME,"form-group")
+                    date = driver.find_element(By.XPATH,'//*[@id="zDiemdanh_style1_ngay"]').get_attribute('value')
+                    print(date)
+                    for form in forms:
+                        labels = form.find_elements(By.XPATH,"./label")
+                        if labels[0].text.strip() == "Chọn lớp" or labels[0].text.strip() == "Ngày":
+                            continue
+                        elif labels[0].text.strip() == "Nhận xét khác/ Other comments":
+                            other_comments = driver.find_element(By.XPATH,'//*[@id="zDiemdanh_style1_comment"]').get_attribute('value')
+                            print("Nhận xét khác/ Other comments:", other_comments)                         
+                        else:
+                            print(labels[0].text)
+                            labels.remove(labels[0])
+                            for label in labels:
+                                print(f'{label.text}: {label.find_element(By.XPATH,"./input").get_attribute("checked")}') 
+                except:
+                    pass
+                pass
         df_data.append(table_row)
     df = pd.DataFrame(df_data,columns=header_row)
     df = df.iloc[: , 1:]
@@ -40,13 +63,12 @@ def html_to_dataframe(table_header, table_data, course_name=None):
         df['Course'] = temp
     return df
 
-@st.cache(allow_output_mutation=True)
 def load_options():
     # initialize the Chrome driver
     options = Options()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.binary_location = GOOGLE_CHROME_BIN
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(chrome_options=options, executable_path=CHROMEDRIVER_PATH)
@@ -58,26 +80,17 @@ def load_options():
     driver.find_element("id","pass").send_keys(MANAGER_PASSWORD)
     # click login button
     driver.find_element(By.XPATH,'//*[@id="login"]/button').click()
-    # navigate to lop hoc
-    driver.get('https://trivietedu.ileader.vn/Default.aspx?mod=lophoc!lophoc')
+    # navigate to diem danh
+    driver.get('https://trivietedu.ileader.vn/Default.aspx?mod=lophoc!diemdanh')
+    
     # pulling the main table
-    table_header = WebDriverWait(driver, 2).until(
+    table_header = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.XPATH,'//*[@id="dyntable"]/thead/tr/th')))
-    table_data = WebDriverWait(driver, 2).until(
+    table_data = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.XPATH,'//*[@id="showlist"]/tr')))
     course_dict = {}
-    course_df = html_to_dataframe(table_header, table_data)
-    for i in range(1,len(course_df)+1):
-        siso_button = WebDriverWait(driver, 2).until(
-                    EC.element_to_be_clickable((By.XPATH, f'//*[@id="showlist"]/tr[{i}]/td[5]/a[1]')))
-        driver.execute_script("arguments[0].click();", siso_button)
-        table_header = WebDriverWait(driver, 3).until(
-                    EC.presence_of_all_elements_located((By.XPATH,'//*[@id="static"]/thead/tr/th')))
-        table_data = WebDriverWait(driver, 3).until(
-                    EC.presence_of_all_elements_located((By.XPATH,'//*[@id="ctl10_container"]/tr')))
-        course_dict[str(course_df.iloc[i-1]['Tên Lớp'])] = html_to_dataframe(table_header, table_data)
-        # navigate to lop hoc
-        driver.get('https://trivietedu.ileader.vn/Default.aspx?mod=lophoc!lophoc')
+    course_df = html_to_dataframe(driver, table_header, table_data)
+    print(course_df)
 
     return driver, course_df, course_dict
 
