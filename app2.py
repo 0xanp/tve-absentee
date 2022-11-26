ff3ff90d7ed096d7c92f31ae669648ee5f46c5a4
Binh An <anbinhpham110799@gmail.com>
2022-11-26 09:55:56 +0700

    core logic for app v2 implemented, ready for testing

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import wx
 import wx.adv
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -93,7 +93,7 @@ class AppFrame(wx.Frame):
         # initialize the Chrome driver
         options = Options()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
@@ -111,15 +111,17 @@ class AppFrame(wx.Frame):
         date_picker = WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.XPATH,'//*[@id="tungay"]')))
         date_picker.clear()
-        date_picker.send_keys(start_date.strftime("%d/%m/%Y"), Keys.ENTER)
+        date_picker.send_keys(start_date.strftime("%d/%m/%Y"))
         # end date picker
         date_picker = WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.XPATH,'//*[@id="denngay"]')))
         date_picker.clear()
-        date_picker.send_keys(end_date.strftime("%d/%m/%Y"), Keys.ENTER)
+        date_picker.send_keys(end_date.strftime("%d/%m/%Y"))
         button = WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.XPATH,'//*[@id="formsearch"]/button'))).click()
-        time.sleep(15)
+        html_table = WebDriverWait(driver, 60).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="dyntable"]/tbody/tr[1]')))
+        time.sleep(20)
         soup = BeautifulSoup(driver.page_source, "lxml")
         table = [table for table in soup.find_all('table')][1]
         table_headings = table.find_all('th')
@@ -135,52 +137,11 @@ class AppFrame(wx.Frame):
         df = df[['Ngày nghỉ', 'Lớp học', 'Họ Tên*']]
         df = self.tidy_split(df, 'Ngày nghỉ', sep=' ,')
         df = df.groupby(['Lớp học','Ngày nghỉ'])['Họ Tên*'].agg(','.join).reset_index()
-        return driver, df
-    
-    def html_to_dict(self, table_data, tmp_table_rows):
-        course_dict = {}
-        # iterate through the table of students with their attendance 
-        # each student has a dictionary of dates where each date has a dictionary
-        # of specific critera of evaluation: attendance, puncutuality, homework,
-        # attitude, and other comments, each criterion sometimes has sub-criteria as 
-        # well
-
-        # "student" here represents a row in the table data pulled using selenium
-        for student in table_data:
-            # each student (row) has a collection of dates (column)
-            dates = student.find_elements(By.XPATH,"./td")
-            # iterate through each date and construct a dictionary of evaluation critera
-            for date in dates:
-                try:
-                    # first "date" is the name of the student
-                    name = date.find_element(By.XPATH, './b').text
-                    # assign each student their own collection of dates
-                    course_dict[name] = {}
-                    print(f"{name}")
-                except:
-                    try:
-                        absent_dates = date.find_elements(By.XPATH,"./div/a/i")
-                        for absent_date in absent_dates:
-                            if absent_date.get_attribute("class") == "fa fa-check-circle fa-red":
-                                absent_date = datetime.strptime(absent_date.get_attribute("id").split("_")[-1],"%d-%m-%Y")
-                                if absent_date.date() >= self.start_date and absent_date.date() <= self.end_date:
-                                    print('found absent date matched')
-                                    for row in tmp_table_rows:
-                                        cells = row.find_elements(By.XPATH,"./td")
-                                        #print(cells[1].text, absent_date.strftime("%d/%m/%Y"))
-                                        if cells[1].text == absent_date.strftime("%d/%m/%Y"):
-                                            print('finding baihoc and comments')
-                                            course_dict[name][absent_date.strftime("%d/%m/%Y")] = []
-                                            course_dict[name][absent_date.strftime("%d/%m/%Y")].append("\n".join([el.text for el in cells[2].find_elements(By.XPATH,"./div/p")]))
-                                            #course_dict[name][absent_date.strftime("%d/%m/%Y")].append(cells[4].find_element(By.XPATH,"./div/").text)
-                                            print(absent_date.strftime('%A'),absent_date.strftime("%d/%m/%Y"))
-                                            print('Bai Hoc:',course_dict[name][absent_date.strftime("%d/%m/%Y")][0])
-                                            #print('Comments:',course_dict[name][absent_date.strftime("%d/%m/%Y")][1])
-                    except:
-                        print(f'error when getting data for {name} on {date.text}')
-                        pass
-                    pass
-        return course_dict
+        df.index +=1
+        driver.get("https://trivietedu.ileader.vn/Default.aspx?mod=lophoc!lophoc_baihoc")
+        course_select = Select(WebDriverWait(driver, 1.5).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="idlophoc"]'))))
+        return driver, course_select, df
     
     def showProgress(self):
         self.progress = wx.ProgressDialog("Pulling Absentee Data in progress...", "Please wait!", maximum=self.maxPercent, parent=self, style=wx.PD_SMOOTH|wx.PD_AUTO_HIDE)
@@ -198,81 +159,41 @@ class AppFrame(wx.Frame):
 
     def OnOkClick(self, evt):
         start_time = datetime.now()
-        driver, df = self.load_options(self.start_date, self.end_date)
+        driver, course_select, df = self.load_options(self.start_date, self.end_date)
         print(df)
-        percent = 0
-        course_dicts = {}
-        """
-        self.maxPercent = len(tmp_course_select.options)
+        self.maxPercent = len(df)
         self.showProgress()
-        for course in tmp_course_select.options:
+        lessons = []
+        for index, row in df.iterrows():
             try:
-                course_select.select_by_visible_text(course.text)
-                print("Getting Absentee data for",course.text)
-                tmp_course_select.select_by_visible_text(course.text)
-                time.sleep(2)
-                tmp_table_rows = WebDriverWait(tmp_driver, 2).until(
-                    EC.presence_of_all_elements_located((By.XPATH,'//*[@id="showlist"]/tr')))
-                if datetime.strptime(tmp_table_rows[0].find_elements(By.XPATH,"./td")[1].text,"%d/%m/%Y").date() > self.end_date or datetime.strptime(tmp_table_rows[-1].find_elements(By.XPATH,"./td")[1].text,"%d/%m/%Y").date() < self.start_date:
-                    percent += 1
-                    self.progress.Update(percent)
+                #print(course_select.all_selected_options[0].text)
+                if course_select.all_selected_options[0].text != row['Lớp học']:
+                    course_select.select_by_visible_text(row['Lớp học'])
+                    time.sleep(1.5)
+                    soup = BeautifulSoup(driver.page_source, "lxml")
+                print(row['Lớp học'])
+                print(row['Ngày nghỉ'])
+                baihoc = [baihoc.next_sibling.text for baihoc in soup.find_all('td',string=row['Ngày nghỉ'])]
+                if baihoc:
+                    print(baihoc[0])
+                    lessons.append(baihoc[0])
+                    self.progress.Update(index)
                 else:
-                    time.sleep(13)
-                    table_data = WebDriverWait(driver, 2).until(
-                        EC.presence_of_all_elements_located((By.XPATH,'//*[@id="showlist"]/tr')))
-                    course_dicts[course.text] = self.html_to_dict(table_data, tmp_table_rows)
-                    percent += 1
-                    self.progress.Update(percent)
+                    lessons.append("")
+                    self.progress.Update(index)
             except:
-                print(f'error when getting data for {course.text}')
-                percent += 1
-                self.progress.Update(percent)
+                print(f"can't select {row['Lớp học']}")
+                lessons.append("")
+                self.progress.Update(index)
                 pass
-        print(course_dicts)
-        df = pd.DataFrame()
-        courses = []
-        names = []
-        dates = []
-        baihoc = []
-        #comments = []
-        for course in course_dicts:
-            for student in course_dicts[course]:
-                if len(course_dicts[course][student]) != 0:
-                    for date in course_dicts[course][student]:
-                        courses.append(course)
-                        names.append(student)
-                        dates.append(date)
-                        baihoc.append(course_dicts[course][student][date][0])
-                        '''
-                        if len(course_dicts[course][student][date]) > 1:
-                            baihoc.append(course_dicts[course][student][date][0])
-                            comments.append(course_dicts[course][student][date][1])
-                        else:
-                            baihoc.append(course_dicts[course][student][date][0])
-                            comments.append("")
-                        '''
-        df["Course Name"] = courses
-        df["Student"] = names
-        df["Absent Date"] = dates
-        df["Bai Hoc"] = baihoc
-        #df["Comments"] = comments
-        df = df.groupby(["Course Name","Absent Date","Bai Hoc"])['Student'].agg(','.join).reset_index()
-        df = df[["Course Name", "Absent Date", "Student", "Bai Hoc"]]
-        df = df.sort_values(['Course Name','Absent Date'], ascending=True).reset_index(drop=True)
-        df.index += 1
+        df['Lesson'] = lessons
         # Create a Pandas Excel writer using XlsxWriter as the engine.
         with pd.ExcelWriter(f"Absentee-{self.start_date}-{self.end_date}.xlsx", engine='xlsxwriter') as writer:
             # Write each dataframe to a different worksheet.
             df.to_excel(writer, sheet_name='Sheet1')
             # Close the Pandas Excel writer and output the Excel file to the buffer
             writer.save()
-        '''
-        with open(f"{self.start_date}-{self.end_date}.json", "w") as outfile:
-            json.dump(course_dicts, outfile)
-        '''
         driver.quit()
-        tmp_driver.quit()
-        """
         end_time = datetime.now()
         print('Duration: {}'.format(end_time - start_time))
         
